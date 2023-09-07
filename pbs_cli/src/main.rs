5f57e2c10390ab0650127fc8d21a0e32310e0f3b
Jérémie Fouché <jeremie.fouche@gmail.com>
2023-09-07 20:45:45 +0200

    use clap

--- pbs_cli/src/main.rs
+++ pbs_cli/src/main.rs
@@ -1,35 +1,40 @@
-mod cmdline;
-
-use cmdline::*;
 use pbs_core::{Result, Store};
 
-fn main() {
-    match parse_cmd_line(std::env::args()) {
-        Ok(command) => match command {
-            Commands::Usage => usage(),
-            Commands::Add(params) => {
-                println!("  CMD ADD: {:?}", params);
-                if let Err(err) = handle_cmd_add(params) {
-                    eprintln!("ERROR : {:?}", err);
-                }
-            }
-        },
-        Err(err) => {
-            eprintln!("ERROR : {:?}", err);
-            usage();
+use clap::{arg, Command};
+
+fn main() -> Result<()> {
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        Some(("add", sub_matches)) => {
+            let pn = sub_matches
+                .get_one::<String>("PART_NUMBER")
+                .expect("required");
+            add_item(pn)?;
         }
+        _ => unreachable!(),
     }
+    Ok(())
 }
 
-fn usage() {
-    eprintln!("Usage");
+fn cli() -> Command {
+    // https://docs.rs/clap/latest/clap/_derive/_cookbook/git/index.html
+    Command::new("pbs")
+        .about("Product Breakdown Store")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("add")
+                .about("Add a new item to store")
+                .arg(arg!(<PART_NUMBER> "Part number"))
+                .arg_required_else_help(true),
+        )
 }
 
-fn handle_cmd_add(params: AddParams) -> Result<()> {
+fn add_item(pn: &str) -> Result<()> {
     let mut store = Store::open("store.db3")?;
     let mut item = store.new_item()?;
-    item.pn = params.pn;
-    item.name = params.name;
+    item.pn = pn.to_string();
     store.save_item(item)?;
     Ok(())
 }
